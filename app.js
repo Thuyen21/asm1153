4170513b94bcd61640677133e684b79f5e6a8a06
Thuyen21 <65522631+Thuyen21@users.noreply.github.com>
2020-06-19 12:13:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var http = require("http");
 var port = process.env.PORT || 3000;
 
+const express = require("express");
+const engines = require("consolidate");
+const app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var publicDir = require("path").join(__dirname, "/public");
+app.use(express.static(publicDir));
+
+//npm i handlebars consolidate --save
+app.engine("hbs", engines.handlebars);
+app.set("views", "./views");
+app.set("view engine", "hbs");
+
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
